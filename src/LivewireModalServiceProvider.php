e0993e274ae10e887d7162d9bf7acab5de7c20a2
wahdam <wahdam.amarulloh@gmail.com>
2022-01-09 17:26:47 +0700

    inital

---
+++ src/LivewireModalServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wahdam\LivewireModal;
+
+use Illuminate\Support\ServiceProvider;
+
+class LivewireModalServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
